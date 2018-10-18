d7091a3d0c88f33a64cc4fdc5efd4589c41381bc
David Richolm <david.richolm@gmail.com>
2018-10-18 16:44:55 +0200

    [fix] Fixed lint error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthModule } from '@/auth/auth.module';
 import { SharedModule } from '@/shared/shared.module';
 import { SettingsModule } from '@/settings/settings.module';
-import { UserModule } from './user/user.module';
-
+import { UserModule } from '@/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule, SharedModule, SettingsModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    AuthModule,
+    SharedModule,
+    SettingsModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
